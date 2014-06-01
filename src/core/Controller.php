206ccefc317eef4ba32409656a6b7ba26c17b848
Angel Barrientos <abarrientos@nyxtechnology.com>
2014-05-31 19:32:53 -0500

    Se crea una clase para el uso de memcached y se implementa como prueba en las plantillas.

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -1,38 +1,48 @@
 <?php
 namespace core;
 use core\http\Request;
-use core\settings\utils\Object;
+use core\settings\utils\Cache;
 use \Exception;
 /**
- * 
+ *
  * @abstract
  * @package core
- * @version 0.1
- * @author Angel Barrientos <uetiko@nyxtechnology.com>
+ * @version 0.1.6
+ * @author Angel Barrientos <uetiko@gmail.com>
  */
-abstract class Controller extends Object{
+abstract class Controller {
 
     private $module = NULL;
     private $request = NULL;
+    private $cache  = null;
+    private $pantilla = null;
 
     public function __construct($module, Request $request) {
         $this->module = $module;
         $this->request = $request;
+        $this->cache = Cache::getInstance();
     }
     /**
-     * 
+     * Imprime la vista con los parametro
      * @param string $view
      * @param array $vars
      */
     protected final function render($view, array $vars = NULL) {
         $params = array();
+        $view = $this->viewExists($view);
+        if(is_null($view)){
+            //no implement yet
+        }
         try {
-            
+            $pantilla = $this->getViewToCache($view);
+            if(!$pantilla){
+                $pantilla = file_get_contents($view);
+                $this->saveViewInCache($view, $pantilla);
+            }
         } catch (Exception $exc) {
             error_log($exc->getTraceAsString(), 0);
         }
-        $view = $this->viewExists($view);
-        $pantilla = file_get_contents($view);
+
         if (settype($vars, 'array') && !is_null($vars)) {
             foreach ($vars as $key => $value) {
                 $params["{{ $key }}"] = $value;
@@ -41,7 +51,10 @@ abstract class Controller extends Object{
         }
         print $pantilla;
     }
-
+    /**
+     * comprueba que la vista solicitada exista
+     * @var string $view
+     */
     private function viewExists($view) {
         $path = realpath(__DIR__ . "/../appmodules/{$this->module}/view/");
         $file = "$path/$view.html";
@@ -51,7 +64,24 @@ abstract class Controller extends Object{
             return NULL;
         }
     }
+    /**
+     * Regresa el objeto core\http\Request
+     * @return core\http\Request $request
+     */
+    protected function getRequest(){
+        return $this->request;
+    }
+
+    private function saveViewInCache($view, $plantilla){
+        $this->cache->setToCache($view,$plantilla, 'core', 15);
+    }
+
+    private function getViewToCache($view){
+        return $this->cache->getToCache($view, 'core');
+    }
+
+    public function __destruct(){
+
+    }
 
 }
-
-?>
---
+++ src/core/settings/utils/Cache.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uetiko
+ * Date: 30/05/14
+ * Time: 22:31
+ */
+
+namespace core\settings\utils;
+use core\settings\utils\Object;
+
+/**
+ * Class Cache
+ * @package core\settings\utils
+ * @author Angel Barrientos <uetiko@gmail.com>
+ * @version 0.1
+ */
+class Cache extends Object{
+    /**
+     * @var Cache
+     */
+    static private $INSTANCE = NULL;
+    /**
+     * @var \Memcache
+     */
+    protected $memcache = null;
+
+    private final function __construct(){
+        $this->memcache = new \Memcache();
+        $this->memcache->pconnect('localhost', 11211);
+    }
+
+    /**
+     * @return Cache
+     */
+    static public function getInstance(){
+        if(!self::$INSTANCE instanceof Cache){
+            self::$INSTANCE = new Cache();
+        }
+        return self::$INSTANCE;
+    }
+
+    /**
+     * @param $k key
+     * @param $v value
+     * @param $prefix prefix to key
+     * @param null $expirtion time in seconds
+     */
+    final public function setToCache($k,$v, $prefix, $expirtion = null){
+        if(is_null($expirtion)){
+            $expirtion = 0;
+        }
+        $this->memcache->set("{$prefix}_{$k}", $v, $expirtion);
+    }
+
+    /**
+     * @param $K
+     * @param $prefix
+     * @return array|string
+     */
+    final public function getToCache($K, $prefix){
+        return $this->memcache->get("{$prefix}_{$K}");
+    }
+    final public function __destruct(){
+        try{
+            $this->memcache->close();
+        }catch (\Exception $e){
+            error_log($e->getTraceAsString(), 0);
+        }
+    }
+}
